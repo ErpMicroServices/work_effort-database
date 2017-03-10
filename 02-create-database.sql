8ae0a03e33bcbdb5b2930ca7ecebc566375511a0
Jim Barrows <Jim.Barrows@gmail.com>
2017-03-10 16:29:00 -0700

    work effort type standards

--- 02-create-database.sql
+++ 02-create-database.sql
@@ -11,38 +11,10 @@ create table if not exists deliverable(
   CONSTRAINT deliverable_pk PRIMARY key(id)
 );
 
-create table if not exists work_effort_type(
-  id uuid DEFAULT uuid_generate_v4(),
-  description text not null CONSTRAINT work_effort_type_description_not_empty CHECK (description <> ''),
-  CONSTRAINT work_effort_type_pk PRIMARY key(id)
-);
-
 create table if not exists work_effort_purpose_type(
   id uuid DEFAULT uuid_generate_v4(),
   description text not null CONSTRAINT work_effort_purpose_type_description_not_empty CHECK (description <> ''),
   CONSTRAINT work_effort_purpose_type_type_pk PRIMARY key(id)
-);
-
-create table if not exists work_effort(
-  id uuid DEFAULT uuid_generate_v4(),
-  name text not null CONSTRAINT work_effort_name_not_empty CHECK (name <> ''),
-  description text,
-  scheduled_start_date date,
-  scheduled_completion_date date,
-  total_expenditure_allowed double precision,
-  total_hours_allowed bigint,
-  estimate_hourse bigint,
-  actual_start timestamp,
-  actual_completion timestamp,
-  actual_hours double precision,
-  special_terms text,
-  quantity_to_produce bigint,
-  quantity_produced bigint,
-  quantity_rejected bigint,
-  work_effort_type_id uuid not null references work_effort_type(id),
-  work_effort_purpose_type_id uuid not null references work_effort_purpose_type(id),
-  a_version_of uuid not null references work_effort(id),
-  CONSTRAINT work_effort_pk PRIMARY key(id)
 );
 
 create table if not exists work_effort_association_type(
@@ -51,31 +23,10 @@ create table if not exists work_effort_association_type(
   CONSTRAINT work_effort_association_type_pk PRIMARY key(id)
 );
 
-create table if not exists work_effort_association(
-  id uuid DEFAULT uuid_generate_v4(),
-  from_date date not null default current_date,
-  thru_date date,
-  associated_from uuid not null references work_effort(id),
-  associated_to uuid not null references work_effort(id),
-  CONSTRAINT work_effort_association_pk PRIMARY key(id)
-);
-
 create table if not exists work_effort_role_type(
   id uuid DEFAULT uuid_generate_v4(),
   description text not null CONSTRAINT work_effort_role_type_description_not_empty CHECK (description <> ''),
   CONSTRAINT work_effort_role_type_pk PRIMARY key(id)
-);
-
-create table if not exists work_effort_party_assignment(
-  id uuid DEFAULT uuid_generate_v4(),
-  from_date date not null default current_date,
-  thru_date date,
-  comment text,
-  assigned_to_party_id uuid not null,
-  assigned_at_facility_id uuid,
-  work_effort_role_type uuid not null references work_effort_role_type(id),
-  work_effort_id uuid not null references work_effort(id),
-  CONSTRAINT work_effort_party_assignment_pk PRIMARY key(id)
 );
 
 create table if not exists work_effort_status_type(
@@ -129,17 +80,6 @@ create table if not exists timesheet_role(
   CONSTRAINT timesheet_role_pk PRIMARY key(id)
 );
 
-create table if not exists time_entry(
-  id uuid DEFAULT uuid_generate_v4(),
-  from_datetime timestamp not null default current_timestamp,
-  thru_datetime timestamp,
-  hours double precision,
-  comment text,
-  timesheet_id uuid not null references timesheet(id),
-  spent_on uuid not null references work_effort(id),
-  CONSTRAINT time_entry_pk PRIMARY key(id)
-);
-
 create table if not exists position_type(
   id uuid DEFAULT uuid_generate_v4(),
   description text not null CONSTRAINT position_type_description_not_empty CHECK (description <> ''),
@@ -171,40 +111,10 @@ create table if not exists party_rate(
   CONSTRAINT party_rate_pk PRIMARY key(id)
 );
 
-create table if not exists work_effort_assignment_rate(
-  id uuid DEFAULT uuid_generate_v4(),
-  from_date date not null default current_date,
-  thru_date date,
-  rate double precision,
-  work_effort_party_assignment_id uuid not null references work_effort_party_assignment(id),
-  rate_type_id uuid not null references rate_type(id),
-  CONSTRAINT work_effort_assignment_rate_pk PRIMARY key(id)
-);
-
-create table if not exists wok_effort_inventory_assignment(
-  id uuid DEFAULT uuid_generate_v4(),
-  quantity bigint not null,
-  work_effort_id uuid not null references work_effort(id),
-  inventory_id uuid not null,
-  CONSTRAINT wok_effort_inventory_assignment_pk PRIMARY key(id)
-);
-
 create table if not exists work_effort_asset_assign_status_type(
   id uuid DEFAULT uuid_generate_v4(),
   description text not null CONSTRAINT work_effort_asset_assign_status_type_description_not_empty CHECK (description <> ''),
   CONSTRAINT work_effort_asset_assign_status_type_pk PRIMARY key(id)
-);
-
-create table if not exists work_effort_fixed_asset_assignment(
-  id uuid DEFAULT uuid_generate_v4(),
-  from_date date not null default current_date,
-  thru_date date,
-  allocated_cost double precision,
-  comment text,
-  work_effort_asset_assign_status_type_id uuid not null references work_effort_asset_assign_status_type(id),
-  fixed_asset_id uuid not null,
-  work_effort_id uuid not null references work_effort(id),
-  CONSTRAINT work_effort_fixed_asset_assignment_pk PRIMARY key(id)
 );
 
 create table if not exists fixed_asset_type(
@@ -241,4 +151,140 @@ create table if not exists party_fixed_asset_assignment(
   comment text,
   party_asset_assignment_status_type_id uuid not null references party_asset_assignment_status_type(id),
   CONSTRAINT party_fixed_asset_assignment_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_type(
+  id uuid DEFAULT uuid_generate_v4(),
+  description text not null CONSTRAINT work_effort_type_description_not_empty CHECK (description <> ''),
+  standard_work_hours double precision not null default 1,
+  fixed_asset_type_id uuid references fixed_asset_type(id),
+  deliverable_type_id uuid references deliverable_type(id),
+  product_id uuid,
+  CONSTRAINT work_effort_type_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort(
+  id uuid DEFAULT uuid_generate_v4(),
+  name text not null CONSTRAINT work_effort_name_not_empty CHECK (name <> ''),
+  description text,
+  scheduled_start_date date,
+  scheduled_completion_date date,
+  total_expenditure_allowed double precision,
+  total_hours_allowed bigint,
+  estimate_hourse bigint,
+  actual_start timestamp,
+  actual_completion timestamp,
+  actual_hours double precision,
+  special_terms text,
+  quantity_to_produce bigint,
+  quantity_produced bigint,
+  quantity_rejected bigint,
+  work_effort_type_id uuid not null references work_effort_type(id),
+  work_effort_purpose_type_id uuid not null references work_effort_purpose_type(id),
+  a_version_of uuid not null references work_effort(id),
+  CONSTRAINT work_effort_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_type_assocation_type(
+  id uuid DEFAULT uuid_generate_v4(),
+  description text not null CONSTRAINT work_effort_type_assocation_type_description_not_empty CHECK (description <> ''),
+  CONSTRAINT work_effort_type_assocation_type_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_type_association(
+  id uuid DEFAULT uuid_generate_v4(),
+  from_work_effort_type_id uuid not null references work_effort_type(id),
+  to_work_effort_type_id uuid not null references work_effort_type(id),
+  CONSTRAINT _pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_skill_standard(
+  id uuid DEFAULT uuid_generate_v4(),
+  estimated_number_of_people bigint not null default 1,
+  estimated_duration double precision,
+  esitmated_cost double precision,
+  for_work_effort_type_id uuid not null references work_effort_type(id),
+  for_skill_type_id uuid not null references skill_type(id),
+  CONSTRAINT work_effort_skill_standard_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_good_standard(
+  id uuid DEFAULT uuid_generate_v4(),
+  estimated_quantity bigint ,
+  estimated_cost double precision,
+  for_work_effort_type_id uuid not null references work_effort_type(id),
+  for_good_id uuid not null,
+  CONSTRAINT work_effort_good_standard_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_good_standard(
+  id uuid DEFAULT uuid_generate_v4(),
+  estimated_quantity bigint ,
+  estimated_duration double precision,
+  estimated_cost double precision,
+  for_work_effort_type_id uuid not null references work_effort_type(id),
+  for_fixed_asset_id uuid not null references fixed_asset(id),
+  CONSTRAINT work_effort_good_standard_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_association(
+  id uuid DEFAULT uuid_generate_v4(),
+  from_date date not null default current_date,
+  thru_date date,
+  associated_from uuid not null references work_effort(id),
+  associated_to uuid not null references work_effort(id),
+  CONSTRAINT work_effort_association_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_party_assignment(
+  id uuid DEFAULT uuid_generate_v4(),
+  from_date date not null default current_date,
+  thru_date date,
+  comment text,
+  assigned_to_party_id uuid not null,
+  assigned_at_facility_id uuid,
+  work_effort_role_type uuid not null references work_effort_role_type(id),
+  work_effort_id uuid not null references work_effort(id),
+  CONSTRAINT work_effort_party_assignment_pk PRIMARY key(id)
+);
+
+create table if not exists time_entry(
+  id uuid DEFAULT uuid_generate_v4(),
+  from_datetime timestamp not null default current_timestamp,
+  thru_datetime timestamp,
+  hours double precision,
+  comment text,
+  timesheet_id uuid not null references timesheet(id),
+  spent_on uuid not null references work_effort(id),
+  CONSTRAINT time_entry_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_assignment_rate(
+  id uuid DEFAULT uuid_generate_v4(),
+  from_date date not null default current_date,
+  thru_date date,
+  rate double precision,
+  work_effort_party_assignment_id uuid not null references work_effort_party_assignment(id),
+  rate_type_id uuid not null references rate_type(id),
+  CONSTRAINT work_effort_assignment_rate_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_inventory_assignment(
+  id uuid DEFAULT uuid_generate_v4(),
+  quantity bigint not null,
+  work_effort_id uuid not null references work_effort(id),
+  inventory_id uuid not null,
+  CONSTRAINT wok_effort_inventory_assignment_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_fixed_asset_assignment(
+  id uuid DEFAULT uuid_generate_v4(),
+  from_date date not null default current_date,
+  thru_date date,
+  allocated_cost double precision,
+  comment text,
+  work_effort_asset_assign_status_type_id uuid not null references work_effort_asset_assign_status_type(id),
+  fixed_asset_id uuid not null,
+  work_effort_id uuid not null references work_effort(id),
+  CONSTRAINT work_effort_fixed_asset_assignment_pk PRIMARY key(id)
 );
