8a1693fd4a8658c5570ce8c090da47407e5254dd
Jim Barrows <Jim.Barrows@gmail.com>
2017-03-10 15:29:43 -0700

    work effort party assignment

--- 02-create-database.sql
+++ 02-create-database.sql
@@ -41,5 +41,66 @@ create table if not exists work_effort(
   quantity_rejected bigint,
   work_effort_type_id uuid not null references work_effort_type(id),
   work_effort_purpose_type_id uuid not null references work_effort_purpose_type(id),
-  CONSTRAINT _pk PRIMARY key(id)
+  a_version_of uuid not null references work_effort(id),
+  CONSTRAINT work_effort_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_association_type(
+  id uuid DEFAULT uuid_generate_v4(),
+  description text not null CONSTRAINT work_effort_association_type_description_not_empty CHECK (description <> ''),
+  CONSTRAINT work_effort_association_type_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_association(
+  id uuid DEFAULT uuid_generate_v4(),
+  from_date date not null default current_date,
+  thru_date date,
+  associated_from uuid not null references work_effort(id),
+  associated_to uuid not null references work_effort(id),
+  CONSTRAINT work_effort_association_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_role_type(
+  id uuid DEFAULT uuid_generate_v4(),
+  description text not null CONSTRAINT work_effort_role_type_description_not_empty CHECK (description <> ''),
+  CONSTRAINT work_effort_role_type_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_party_assignment(
+  id uuid DEFAULT uuid_generate_v4(),
+  from_date date not null default current_date,
+  thru_date date,
+  comment text,
+  assigned_to_party_id uuid not null,
+  assigned_at_facility_id uuid,
+  work_effort_role_type uuid not null references work_effort_role_type(id),
+  work_effort_id uuid not null references work_effort(id),
+  CONSTRAINT work_effort_party_assignment_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_status_type(
+  id uuid DEFAULT uuid_generate_v4(),
+  description text not null CONSTRAINT work_effort_status_type_description_not_empty CHECK (description <> ''),
+  CONSTRAINT work_effort_status_type_pk PRIMARY key(id)
+);
+
+create table if not exists work_effort_status(
+  id uuid DEFAULT uuid_generate_v4(),
+  datetime timestamp not null default CURRENT_TIMESTAMP,
+  work_effort_status_type_id uuid not null references work_effort_status_type(id),
+  CONSTRAINT work_effort_status_pk PRIMARY key(id)
+);
+
+create table if not exists skill_type(
+  id uuid DEFAULT uuid_generate_v4(),
+  description text not null CONSTRAINT _type_description_not_empty CHECK (description <> ''),
+  CONSTRAINT skill_type_type_pk PRIMARY key(id)
+);
+
+create table if not exists party_skill(
+  id uuid DEFAULT uuid_generate_v4(),
+  years_experience bigint,
+  rating text,
+  skill_type_id uuid not null references skill_type(id),
+  CONSTRAINT party_skill_pk PRIMARY key(id)
 );
--- 03-initial-data.sql
+++ 03-initial-data.sql
@@ -8,3 +8,7 @@ insert into work_effort_purpose_type (description) values ('Production Run');
 insert into work_effort_purpose_type (description) values ('Maintenance');
 insert into work_effort_purpose_type (description) values ('Work Flow');
 insert into work_effort_purpose_type (description) values ('Research');
+
+insert into work_effort_association_type (description) values ('Work Effort Breakdown');
+insert into work_effort_association_type (description) values ('Work Effort Precedency');
+insert into work_effort_association_type (description) values ('Work Effort Concurrency');
